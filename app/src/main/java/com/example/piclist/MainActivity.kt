cf7ca64991b2725fca3a017e49082a0d4336e56e
bekka <beqachachua@gmail.com>
2021-09-04 20:23:52 +0400

    # add introHeader and box selection

--- app/src/main/java/com/example/piclist/MainActivity.kt
+++ app/src/main/java/com/example/piclist/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.piclist.ui.HomeScreen
 import com.example.piclist.ui.theme.PiclistTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +16,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PiclistTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PiclistTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/piclist/ui/HomeScreen.kt
+++ app/src/main/java/com/example/piclist/ui/HomeScreen.kt
@@ -1,13 +1,26 @@
 package com.example.piclist.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.piclist.R
+import com.example.piclist.ui.theme.ButtonBlue
+import com.example.piclist.ui.theme.DarkerButtonBlue
 import com.example.piclist.ui.theme.DeepBlue
+import com.example.piclist.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen() {
@@ -16,9 +29,64 @@ fun HomeScreen() {
             .background(DeepBlue)
             .fillMaxSize()
     ) {
-        Column() {
-
+        Column {
+            introHeader()
+            itemSelection(item = listOf("Sweet sleep","Insomnia","Depression"))
         }
         //Navigation
+    }
+}
+
+@Composable
+fun introHeader(
+    name: String = "Beka"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Good morning, $name", style = MaterialTheme.typography.h2)
+            Text(text = "We wish you have a good day!", style = MaterialTheme.typography.body1)
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
+
+@Composable
+fun itemSelection(
+    item: List<String>
+) {
+    var selectedItemIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(item.size) {
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedItemIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedItemIndex == it) ButtonBlue
+                        else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            ) {
+                Text(text = item[it], color = TextWhite)
+            }
+        }
     }
 }
