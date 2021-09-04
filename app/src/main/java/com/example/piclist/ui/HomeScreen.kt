05f88ebc409b8d028ac4a9ee53e2fe98978113de
bekka <beqachachua@gmail.com>
2021-09-04 22:06:21 +0400

    # add feature item

--- app/src/main/java/com/example/piclist/ui/HomeScreen.kt
+++ app/src/main/java/com/example/piclist/ui/HomeScreen.kt
@@ -1,10 +1,13 @@
 package com.example.piclist.ui
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
@@ -14,12 +17,19 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.piclist.R
+import com.example.piclist.ui.extenstions.standardQuadFromTo
+import com.example.piclist.ui.model.FeatureData
 import com.example.piclist.ui.theme.*
 
+@ExperimentalFoundationApi
 @Composable
 fun HomeScreen() {
     Box(
@@ -29,8 +39,38 @@ fun HomeScreen() {
     ) {
         Column {
             IntroHeader()
-            ItemSelection(item = listOf("Sweet sleep","Insomnia","Depression"))
+            ItemSelection(item = listOf("Sweet sleep", "Insomnia", "Depression"))
             CurrentPlayer()
+            FeatureSection(features = listOf(
+                FeatureData(
+                    title = "Sleep meditation",
+                    R.drawable.ic_play,
+                    BlueViolet1,
+                    BlueViolet2,
+                    BlueViolet3
+                ),
+                FeatureData(
+                    title = "Tips for sleeping",
+                    R.drawable.ic_play,
+                    LightGreen1,
+                    LightGreen2,
+                    LightGreen3
+                ),
+                FeatureData(
+                    title = "Night island",
+                    R.drawable.ic_play,
+                    OrangeYellow1,
+                    OrangeYellow2,
+                    OrangeYellow3
+                ),
+                FeatureData(
+                    title = "Calming sounds",
+                    R.drawable.ic_play,
+                    Beige1,
+                    Beige2,
+                    Beige3
+                )
+            ))
         }
         //Navigation
     }
@@ -106,7 +146,11 @@ fun CurrentPlayer(
     ) {
         Column {
             Text(text = "Daily Thought", style = MaterialTheme.typography.h2)
-            Text(text = "Meditation ⁘ 3-10 min", style = MaterialTheme.typography.body1, color = TextWhite)
+            Text(
+                text = "Meditation ⁘ 3-10 min",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
         }
         Box(
             contentAlignment = Alignment.Center,
@@ -121,6 +165,124 @@ fun CurrentPlayer(
                 contentDescription = "Play",
                 tint = Color.White,
                 modifier = Modifier.size(16.dp)
+            )
+        }
+    }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun FeatureSection(features: List<FeatureData>) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Features", style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(15.dp)
+        )
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(features.size) {
+                FeatureItem(feature = features[it])
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: FeatureData
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+            standardQuadFromTo(mediumColoredPoint1,mediumColoredPoint2)
+            standardQuadFromTo(mediumColoredPoint2,mediumColoredPoint3)
+            standardQuadFromTo(mediumColoredPoint3,mediumColoredPoint4)
+            standardQuadFromTo(mediumColoredPoint4,mediumColoredPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        val lightPoint1 = Offset(0f, height * 0.35f)
+        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+        val lightColoredPath = Path().apply {
+            moveTo(lightPoint1.x, lightPoint1.y)
+            standardQuadFromTo(lightPoint1, lightPoint2)
+            standardQuadFromTo(lightPoint2, lightPoint3)
+            standardQuadFromTo(lightPoint3, lightPoint4)
+            standardQuadFromTo(lightPoint4, lightPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            drawPath(
+                path = mediumColoredPath,
+                color = feature.mediumColor
+            )
+            drawPath(
+                path = lightColoredPath,
+                color = feature.lightColor
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = MaterialTheme.typography.h2,
+                lineHeight = 26.sp,
+                modifier = Modifier.align(Alignment.TopStart)
+            )
+            Icon(
+                painter = painterResource(id = feature.iconId),
+                contentDescription = feature.title,
+                tint = Color.White,
+                modifier = Modifier.align(Alignment.BottomStart)
+            )
+            Text(
+                text = "Start",
+                color = TextWhite,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .clickable {
+                        // Handle the click
+                    }
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(ButtonBlue)
+                    .padding(vertical = 6.dp, horizontal = 15.dp)
             )
         }
     }
