0fb3fc489c80ce7e2d4a81a224fa977c6557b4da
bekka <beqachachua@gmail.com>
2021-09-04 18:49:55 +0400

    # create home screen

---
+++ app/src/main/java/com/example/piclist/ui/HomeScreen.kt
@@ -0,0 +1,24 @@
+package com.example.piclist.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.piclist.ui.theme.DeepBlue
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column() {
+
+        }
+        //Navigation
+    }
+}
